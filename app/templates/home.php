ae6bcd320a7fd7638817be3b70f441462f8681e6
erronjason <ejsayre@gmail.com>
2014-09-16 08:32:17 -0400

    More colors

--- app/templates/home.php
+++ app/templates/home.php
@@ -1,7 +1,9 @@
 <?php include PARTIALS_DIR.'/header.php'; ?>
 <div id="wrapper">
     <div id="content">
+
         <div id="main">
+            <h1><?php if ($headercontent) echo $headercontent; ?></h1>
             <?php if ($content) echo $content; ?>
         </div>
     </div>
--- app/views/home.php
+++ app/views/home.php
@@ -1,6 +1,7 @@
 <?php
 
 $head['title'] = 'Jason Sayre';
+$headercontent = "Header";
 $content = 'Main body text. Lots of test text. Lorem ipsum dolor sit amet, blah blah blah blah.';
 
 include TEMPLATE_DIR.'/home.php';
